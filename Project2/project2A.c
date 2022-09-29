1ae3af7608dd959337a369f891b26e18671217ae
AndrewSushi <88579779+AndrewSushi@users.noreply.github.com>
2022-09-28 20:37:31 -0700

    Add files via upload

---
+++ Project2/project2A.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void odd_composites(int a, int b){
+	for(int i = a; i<= b; i++){
+		if(i % 2 == 0){
+			continue;
+		}
+		for(int j = 2; j<=i/2; j++){
+			if(i % j == 0){
+				printf("%d is a composite number.\n", i);
+				break;
+			}
+		}
+	}
+}
+
+int main(){
+	odd_composites(10, 500);
+}
+
+
