fdb9503626dbc6de8f9174069054b25f12a4ccec
Andrew Chan <88579779+AndrewSushi@users.noreply.github.com>
2022-11-22 03:30:31 -0800

    Add files via upload

--- Project3/proj3C.c
+++ Project3/proj3C.c
@@ -9,11 +9,12 @@
 #include <string.h>  /* strcmp */
 
 /* Do not change these values for your submission. */
-#define QUEUE_SIZE 16  /* Note: Works with 12 on 3C-input.txt */
+#define QUEUE_SIZE 12  /* Note: Works with 12 on 3C-input.txt */
 #define BLOOD_TYPES 4
 #define LINE_LENGTH 80
 
-/* Your code goes below this line. */
+int currentDonors = 0; // Since there are multiple blood types this keeps track of all
+int currentRecips = 0;
 
 struct queue
 {
@@ -21,7 +22,6 @@ struct queue
 	int back;
 	char *strings[LINE_LENGTH];
 	int population;
-  /* Add your struct members here. */
 };
 typedef struct queue Queue;
 
@@ -43,9 +43,6 @@ char *dequeue(Queue *q){
 	return dequeued;
 	
 }
-/* Your code goes above this line. */
-
-
 /* Here is a suggested helper function for checking blood type
    compatibility. */
 int isDonorToRecipient(char *donor, char *recipient)
@@ -59,9 +56,179 @@ int isDonorToRecipient(char *donor, char *recipient)
   return 0;
 }
 
+int getBloodIndex(char bloodType){
+	switch (bloodType){
+		case 'X':
+			return 0;
+		case 'B':
+			return 1;
+		case 'A':
+			return 2;
+		case 'O':
+			return 3;
+	}
+}
 
-/* Uncomment printQueue and prettyPrintQueue when your queue struct
-   and functions are implemented. */
+char getBloodType(char *person){
+	person += 2; // skip the first two characters
+	if(*person == 'O' || *person == 'B'){
+		return *person;
+	}else if(person[1] == ':'){
+		return 'A';
+	}else{
+		return 'X'; // 'X' is blood type AB
+	}
+}
+
+void printMatch(char *don, char *rec, char *sur){
+	printf("MATCH: %s donates to %s via Dr. %s\n", don, rec, sur);
+}
+
+char *getName(char *person){
+	int colonCounter = 0;
+	int i = 0;
+	char *name;
+	if(person[0] != 'S'){
+		while(colonCounter != 2){ // Renmove Colons
+			if(*person == ':'){
+				colonCounter++;
+			}
+			person++;
+		}
+	}else{
+		while(colonCounter != 1){ // Renmove Colons
+			if(*person == ':'){
+				colonCounter++;
+			}
+			person++;
+		}
+	}
+	char *temp = person;
+	while(*person != '\n'){
+		*person = *person;
+		person++;
+	}
+	*person = '\0';
+	person = temp;
+	return person;
+}
+
+void printQueue(struct queue *q);
+
+int isValidSurgery(char don, char rec){
+	if(don == 'O' && don == rec){ // Both O
+		return 1;
+	}
+
+}
+
+int findPriority(Queue **rec, Queue **don, char blood, int isDonor){ // Returns the index corresponding to bloodType
+	if(isDonor){
+		if(blood == 'X'){
+			if(rec[0]->population){
+				return 0; // There is an AB donor and Recip
+			}else{
+				return -1; // No AB rec
+			}
+		}else if(blood ==  'B'){
+			if(rec[0]->population){
+				if((rec[1]->population && rec[2]->population && rec[3]->population) == 0){
+					return 0; // Only recip in all queues
+				}
+			}
+			if(rec[1]->population){
+				return 1; // There is a type B;
+			}else{
+				return -1;
+			}
+		}else if(blood == 'A'){	
+			if(rec[0]->population){
+				if((rec[1]->population && rec[2]->population && rec[3]->population) == 0){
+					return 0;
+				}
+			}
+			if(rec[2]->population){
+				return 2; // There is a type A;
+			}else{
+				return -1;
+			}
+		}else{
+			if(rec[0]->population){
+				if((rec[1]->population && rec[2]->population && rec[3]->population) == 0){
+					return 0;
+				}
+			}
+			if(rec[3]->population){
+				return 3; // There is a type A;
+			}else{
+				return -1;
+			}
+		}
+	}
+//	for(int i = 0; i < 4; i++){
+//		printQueue(don[i]);
+//	}
+}
+
+void parseData(char *person, Queue **don, Queue **rec, Queue *sur){
+	char *name = getName(person);
+	if(person[0] == 'R'){
+		char type = getBloodType(person);
+		int idx = getBloodIndex(type);
+		if(sur->population == 0){
+			enqueue(name, rec[idx]);
+			currentRecips++;
+		}else if(currentDonors){
+			if
+//			printMatch(dequeue(don[idx]), name, dequeue(sur));
+//			
+		}else{
+			enqueue(name, rec[idx]);
+			currentRecips++;
+		}
+	}else if(person[0] == 'D'){
+		char type = getBloodType(person);
+		int idx = getBloodIndex(type);
+//		if(sur->population == 0){
+			enqueue(name, don[idx]);
+//		}else if(don[idx]->population){
+//			printMatch(name, dequeue(rec[idx]), dequeue(sur));
+//		}else{
+//			enqueue(name, don[idx]);
+//		}
+	}else{
+//		int ifSurgery = 0;
+//		for(int i = 0; i < 4; i++){
+//			if(don[i]->population && rec[i]->population){
+//				printMatch(dequeue(don[i]), dequeue(rec[i]), name);
+//				printQueue(don[i]);
+//				printQueue(rec[i]);
+//				ifSurgery = 1;
+//				break;
+//			}
+//		}
+//		if(ifSurgery == 0){
+			enqueue(name, sur);
+//		}
+	}
+}
+
+
+void openFile(char *argv, Queue **don, Queue **rec, Queue *sur){
+	FILE *f = fopen(argv, "r");
+	if(f == NULL){
+		printf("File not found in current path.\n");
+		exit(EXIT_FAILURE);
+	}
+	char *linePtr = (char *)malloc(LINE_LENGTH);
+	size_t n = LINE_LENGTH;
+	ssize_t count = getline(&linePtr, &n, f);
+	while(count != -1){
+//		printf("%s\n", linePtr);
+		parseData(linePtr, don, rec, sur);
+		count = getline(&linePtr, &n, f);
+	}
+}
 
 void printQueue(struct queue *q)
 {
@@ -74,15 +241,13 @@ void printQueue(struct queue *q)
   }
   else
   {
-    int i = q->front;
-    while (i != q->back)
+    for (int i = 0; i < q->population; i++)
     {
-      printf("\t\tEntry[%d]: \"%s\"\n", i, q->strings[i]);
-      i = (i + 1) % QUEUE_SIZE;
+      int index = (q->front + i) % QUEUE_SIZE;
+      printf("\t\tEntry[%d]: \"%s\"\n", index, q->strings[index]);
     }
   }
 }
-
 
 void prettyPrintQueue(struct queue *q, char *label, char *type)
 {
@@ -93,34 +258,34 @@ void prettyPrintQueue(struct queue *q, char *label, char *type)
   else
   {
     printf("Unmatched %s (%s):\n", label, type);
-    int i = q->front;
-    while (i != q->back)
+    for (int i = 0; i < q->population; i++)
     {
-      printf("%s\n", q->strings[i]);
-      i = (i + 1) % QUEUE_SIZE;
+      int index = (q->front + i) % QUEUE_SIZE;
+      printf("%s\n", q->strings[index]);
     }
   }
 }
-
-
 
 int main(int argc, char **argv)
 {
   /***  STEP #1: Implement your queue code and test it.  ***/
 
   /* This test code: */
-  
-  struct queue *q = malloc(sizeof (struct queue));
-  initializeQueue(q);
-  printQueue(q);
-  enqueue("hello", q);
-  printQueue(q);
-  enqueue("world", q);
-  printQueue(q);
-  printf("Dequeue: %s\n", dequeue(q));
-  printQueue(q);
-  free(q);
-  
+ 
+/* 
+	struct queue *q = malloc(sizeof (struct queue));
+	initializeQueue(q);
+	printQueue(q);
+	enqueue("hello", q);
+	printQueue(q);
+	enqueue("world", q);
+  	printQueue(q);
+	printf("Dequeue: %s\n", dequeue(q));
+	printQueue(q);
+	free(q);
+*/  
+
+
   /* Gives this output (with different pointers):
 
 Printing queue 0x7fff5e6878a8
@@ -151,7 +316,6 @@ Printing queue 0x7fff5e6878a8
              You must not use fread.
              You should exit with an error if file opening fails.
   ***/
-
   /***
     STEP #3: After your queue code works and after you can read the
              file, implement the prompt.
@@ -172,30 +336,54 @@ Printing queue 0x7fff5e6878a8
   /* Here is a suggested helper array for facilitating matching as
     described in the prompt. The blood types are listed from most
     rare to least rare. */
-  char *types[BLOOD_TYPES] = {"AB", "B", "A", "O"};
+	char *types[BLOOD_TYPES] = {"AB", "B", "A", "O"}; // X represents AB
 
-  struct queue *donors[BLOOD_TYPES];
-  struct queue *recipients[BLOOD_TYPES];
-  struct queue *surgeons = malloc(sizeof *surgeons);
+	struct queue *donors[BLOOD_TYPES];
+	struct queue *recipients[BLOOD_TYPES];
+	struct queue *surgeons = malloc(sizeof *surgeons);
+	initializeQueue(surgeons);
 
+	for(int i = 0; i < 4; i++){
+		donors[i] = (Queue *)malloc(sizeof(Queue));
+		recipients[i] = (Queue *)malloc(sizeof(Queue));
+		initializeQueue(donors[i]);
+		initializeQueue(recipients[i]);	
+	}
+//	printf("%s\n", dequeue(surgeons));	
+	openFile(argv[1], donors, recipients, surgeons);
+//	printQueue(surgeons);
+//	printQueue(donors[0]);
+//	printQueue(recipients[0]);
+//	printQueue(recipients[1]);
+//	printQueue(recipients[2]);
+//	printQueue(recipients[3]);
 
+//	for(int i = 0; i < 4; i++){
+//		printQueue(donors[i]);
+//		printQueue(recipients[i]);
+//	}
+//	printQueue(surgeons);
 
-  /***
+//	findPriority(donors);
+
+  /*
     STEP #4: After your matching code is done, add prettyPrintQueue
              calls for each of your recipient queues, your donor
              queues, and your surgeon queue. If you used the
              suggested "recipients" and "donors" arrays, and the
              "types" array, then you can use these print statements:
+*/
+//	for (int r = 0; r < BLOOD_TYPES; r++)
+//		prettyPrintQueue(recipients[r], "recipients", types[r]);
+//	for (int d = 0; d < BLOOD_TYPES; d++)
+//		prettyPrintQueue(donors[d], "donors", types[d]);
+//		prettyPrintQueue(surgeons, "surgeons", "type-agnostic");
 
-  for (int r = 0; r < BLOOD_TYPES; r++)
-    prettyPrintQueue(recipients[r], "recipients", types[r]);
-  for (int d = 0; d < BLOOD_TYPES; d++)
-    prettyPrintQueue(donors[d], "donors", types[d]);
-  prettyPrintQueue(surgeons, "surgeons", "type-agnostic");
-
+/*
              If you did not use arrays for your recipient and donor
              queues, then you should print your recipient queues in
              this order, for example:
+
 
   prettyPrintQueue(recipients_AB, "recipients", "AB");
   prettyPrintQueue(recipients_A, "recipients", "B");
@@ -204,7 +392,7 @@ Printing queue 0x7fff5e6878a8
 
              Then, print your donor queues in the same order, then
              print your surgeon queue.
-  ***/
+  */
 
   /* If you have time, be sure to free any memory you allocated. */
   free(surgeons);
