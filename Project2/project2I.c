3206e3aa6bf7367b05cf1809d30444652aacdf13
Andrew Chan <88579779+AndrewSushi@users.noreply.github.com>
2022-11-21 23:14:43 -0800

    Add files via upload

---
+++ Project2/project2I.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+
+int *AllocateArray(int N)
+{
+	int *arr = (int *)malloc(sizeof(int) * N);
+	for(int i = 0; i < N; i++){
+		arr[i] = (rand() % (10 * N));
+	}
+	return arr;
+}
+
+int compare(const void *x, const void *y){
+	return (*(int *)x - *(int *)y);
+}
+
+void SortArray(int *A, int N){
+	qsort(A, N, sizeof(int), compare);
+}
+
+void DeallocateArray(int *A){
+	free(A);
+}
+
+int main()
+{
+//	int sizes[8] = { 1000, 2000, 4000, 8000, 16000, 32000, 64000, 128000};
+
+// For fun:
+   int sizes[11] = { 1000, 2000, 4000, 8000, 16000, 32000, 64000, 128000,
+                     256000, 512000, 1024000 };
+  
+
+    for (int i = 0 ; i < 11 ; i++)
+    {
+	double alloc_time = 0., sort_time = 0., dealloc_time = 0.;
+	struct timeval startTime1;
+	gettimeofday(&startTime1, 0);
+	int *arr = AllocateArray(sizes[i]);
+	struct timeval endTime1;
+	gettimeofday(&endTime1, 0);
+	alloc_time = (double)(endTime1.tv_sec - startTime1.tv_sec) + ((double)(endTime1.tv_usec - startTime1.tv_usec) / 1000000.);
+        	
+	struct timeval startTime2;
+	gettimeofday(&startTime2, 0);
+	SortArray(arr, sizes[i]);
+	struct timeval endTime2;
+	gettimeofday(&endTime2, 0);
+	sort_time = (double)(endTime2.tv_sec - startTime2.tv_sec) + ((double)(endTime2.tv_usec - startTime2.tv_usec) / 1000000.);
+		
+	struct timeval startTime3;
+	gettimeofday(&startTime3, 0);
+	DeallocateArray(arr);
+	struct timeval endTime3;
+	gettimeofday(&endTime3, 0);
+	dealloc_time = (double)(endTime3.tv_sec - startTime3.tv_sec) + ((double)(endTime3.tv_usec - startTime3.tv_usec) / 1000000.);
+
+	printf("Timings for array of size %d\n", sizes[i]);        
+	printf("\tTime for allocation is %g, time per element = %g\n", alloc_time, alloc_time/sizes[i]);
+	printf("\tTime for sort_time is %g, time per element = %g\n", sort_time, sort_time/sizes[i]);
+	printf("\tTime for deallocation is %g\n", dealloc_time);
+    }
+}
